63c0ebd0bc73c6e07c484b8f5785408654fc825c
Платон <zhemerov17@mail.ru>
2022-11-23 03:37:56 +0300

    final fix

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -8,15 +8,7 @@ from posts.models import Group, Post
 from .serializers import (
     CommentSerializer, GroupSerializer, PostSerializer
 )
-
-
-class IsAuthorOrReadOnly(permissions.BasePermission):
-
-    message = 'Изменение чужого контента запрещено!'
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user)
+from .permissions import IsAuthorOrReadOnly
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
